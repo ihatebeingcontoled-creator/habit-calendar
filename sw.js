9af2856efdf6a71086c33ccc2dbc599b07a4a98e
ihatebeingcontoled-creator <ihatebeingcontoled@gmail.com>
2026-09-05 09:06:25 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,3 @@
-const CACHE_NAME = 'habit-calendar-offline-test-v1';
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) =>
-      cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json'
-      ])
-    )
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request);
-    })
-  );
-});
-
-
 self.addEventListener('push', (event) => {
   let data = {};
   try { data = event.data.json(); } catch (err) {}
